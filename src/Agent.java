47c64aed5e00b8998fd5e0ea5e0a6b05a9af00ff
DonatLuffy <messi10m9@hotmail.com>
2017-11-10 02:57:30 +0300

    Submission phase 1

--- src/Agent.java
+++ src/Agent.java
@@ -5,54 +5,41 @@
 // NAME:  ...................
 // ID: ...................
 
-import java.awt.Color;
 import java.io.*;
 import java.util.*;
 
 public class Agent {
-	enum Status {
-		EMPTY {
-			char nothing = ' ';
-			char robot = 'R';
-			char treasure = 'T';
-			char robotAndTreasure = 'U';
-		},
-		HOLE {
-			char nothing = 'H';
-			char robot = 'X';
-			char treasure = 'Y';
-			char robotAndTreasure = 'Z';
-		}
-	}
+	// enum Status {
+	// EMPTY {
+	// char nothing = ' ';
+	// char robot = 'R';
+	// char treasure = 'T';
+	// char robotAndTreasure = 'U';
+	// },
+	// HOLE {
+	// char nothing = 'H';
+	// char robot = 'X';
+	// char treasure = 'Y';
+	// char robotAndTreasure = 'Z';
+	// }
+	// }
 
 	public static void main(String[] args) throws FileNotFoundException {
 
-		// int n_args = args.length;
-		// if (n_args!=4) {
-		// System.out.println("ERROR: ILLEGAL NUMBER OF ARGUMENTS.");
-		// System.out.println("Number of arguments must be 4");
-		// return;
-		// }
-		// String mapFile = args[0];
-		// String commandsFile = args[1];
-		// String finalMapFile = args[2];
-		// String logFile = args[3];
+		int n_args = args.length;
+		if (n_args != 4) {
+			System.out.println("ERROR: ILLEGAL NUMBER OF ARGUMENTS.");
+			System.out.println("Number of arguments must be 4");
+			return;
+		}
+		String mapFile = args[0];
+		String commandsFile = args[1];
+		String finalMapFile = args[2];
+		String logFile = args[3];
 
 		// WRITE YOUR CODE DOWN HERE:
-		State inital = new State("simple.map.txt");
-		try{
-			
-		 Scanner in = new Scanner(new File("simple.map.command.txt"));
-
-		 while(in.hasNext()){
-			 inital.writeLogs("logsFilename.txt", inital.doCommandAndLog(in.next()));
-			 inital.display();
-		 }
-		 in.close();
-		}catch(Exception e){
-			System.out.println(e.getStackTrace());
-		}
-
+		State inital = new State(mapFile);
+		inital.run(commandsFile, finalMapFile, logFile);
 
 	}
 
--- src/State.java
+++ src/State.java
@@ -6,7 +6,6 @@
 // ID: ...................
 
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class State {
@@ -15,23 +14,14 @@ public class State {
 	// THAT YOU NEED TO PUT IN A STATE.
 	// YOU SHOULD CHANGE IT:
 
-	enum Status {
-		EMPTY_NOTHING,
-		EMPTY_ROBOT,
-		EMPTY_TREASURE,
-		EMPTY_TREASURE_ROBOT,
-		HOLE_NOTHING,
-		HOLE_ROBOT,
-		HOLE_TREASURE,
-		HOLE_ROBOT_TREASURE
-	}
+	public static int battery = 10;
+	public static final char chargeStation = 'S';
 
 	private int x; // index R at row
 	private int y; // index R at column
 	private char[][] map; // THE MAP
 	private int N;
 	private int M;
-	public Status status;
 
 	// -----------------------------
 
@@ -63,7 +53,6 @@ public class State {
 		} catch (InputMismatchException e) {
 			System.out.println(e.fillInStackTrace());
 		}
-		status = Status.EMPTY_ROBOT;
 	}
 
 	// CONSTRUCTOR 2:
@@ -115,112 +104,206 @@ public class State {
 	// RETURNS BOOLEAN:
 	// TRUE MEANS ACTION WAS APPLIED,
 	// FALSE MEANS ACTOIN COULD NOT AND WAS NOT APPLIED.
-	public boolean action1() {
-		if (x = x) // (...)
-			// do something
-			return false;
-		return true;
-	}
-
-	// ACTION 2
-	public boolean action2() {
-		// ...
-	}
+	// public boolean action1() {
+	// if (x = x) // (...)
+	// // do something
+	// return false;
+	// return true;
+	// }
+	//
+	// // ACTION 2
+	// public boolean action2() {
+	// // ...
+	// }
 
 	// YOU CAN ADD MORE ACTIONS HERE.
 
-	public void changeStatus(Status status){
-		switch(status){
-		case EMPTY_NOTHING: 
-			map[x][y] = ' ';
-			break;
-		case EMPTY_ROBOT: 
-			map[x][y] = 'R';
-			break;
-		case EMPTY_TREASURE:
-			map[x][y] = 'T';
-			break;
-		case EMPTY_TREASURE_ROBOT:
-			map[x][y] = 'U';
-			break;
-		case HOLE_NOTHING:
-			map[x][y] = 'H';
-			break;
-		case HOLE_ROBOT:
-			map[x][y] = 'X';
-			break;
-		case HOLE_TREASURE:
-			map[x][y] = 'Y';
-			break;
-		case HOLE_ROBOT_TREASURE:
-			map[x][y] = 'Z';
-			break;
-			default:
-				System.out.println("Error on change Status");
+	public void run(String command, String finalMap, String log) {
+		try {
+			Scanner in = new Scanner(new File(command));
+			while (in.hasNext() && battery > 0) {
+				writeLogs(log, Booster_moves(in.next()));
+				if (fallInHole())
+					writeLogs(log, "HOLE");
+				if (foundTreasure())
+					writeLogs(log, "GOAL");
+
+			}
+			in.close();
+			FileWriter f = new FileWriter(new File(finalMap));
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < M; j++) {
+					f.write(map[i][j]);
+				}
+				f.write("\n");
+			}
+			f.close();
+		} catch (Exception e) {
+			System.out.println(e.getStackTrace());
 		}
 	}
-	public char currentCell(){
-		return map[x][y];
+
+	public void chargeStation() {
+		if (map[x][y] == 'S')
+			battery = 10;
 	}
+
+	public boolean move_N_B() {
+		battery -= 3;
+		if (move_N() && move_N())
+			return true;
+		else
+			return false;
+	}
+
+	public boolean move_S_B() {
+		battery -= 3;
+		if (move_S() && move_S())
+			return true;
+		else
+			return false;
+	}
+
+	public boolean move_E_B() {
+		battery -= 3;
+		if (move_E() && move_E())
+			return true;
+		else
+			return false;
+	}
+
+	public boolean move_W_B() {
+		battery -= 3;
+		if (move_W() && move_W())
+			return true;
+		else
+			return false;
+	}
+
+	public String Booster_moves(String cmd) {
+		String log = "ERROR";
+		switch (cmd) {
+		case "move-N":
+			if (move_N_B())
+				log = "DONE";
+			else
+				log = "FAIL";
+			break;
+		case "move-S":
+			if (move_S_B())
+				log = "DONE";
+			else
+				log = "FAIL";
+			break;
+		case "move-E":
+			if (move_E_B())
+				log = "DONE";
+			else
+				log = "FAIL";
+			break;
+		case "move-W":
+			if (move_W_B())
+				log = "DONE";
+			else
+				log = "FAIL";
+			break;
+		}
+		return log;
+	}
+
+	public void changeCell() {
+		switch (map[x][y]) {
+		case ' ':
+			map[x][y] = 'R';
+			break;
+		case 'T':
+			map[x][y] = 'U';
+			break;
+		case 'H':
+			map[x][y] = 'X';
+			break;
+		case 'Y':
+			map[x][y] = 'Z';
+			break;
+		case 'X':
+			map[x][y] = 'X';
+			break;
+		}
+	}
+
+	public void changPreviousCell() {
+		switch (map[x][y]) {
+		case 'R':
+			map[x][y] = ' ';
+			break;
+		case 'U':
+			map[x][y] = 'T';
+			break;
+		}
+	}
+
 	// this method return void, it can return boolean
 	public boolean move_N() {
-		if (x != 0) {
-			if (map[x][y] != 'H') {
-				changeStatus(Status.EMPTY_NOTHING);
-				x--;
-				changeStatus(Status.EMPTY_ROBOT);
-				return true;
-			} else {
-				changeStatus(Status.EMPTY_ROBOT);
-				return false;
-			}
-		} else
+		if (x == 0)
 			return false;
+		else if (map[x][y] == 'X' || map[x][y] == 'Z')
+			return false;
+		else if (map[x - 1][y] == 'B')
+			return false;
+		else {
+			changPreviousCell();
+			x--;
+			changeCell();
+			return true;
+		}
 	}
 
 	// this method return void, it can return boolean
 	public boolean move_S() {
-		if (x != N) {
-			if (map[x][y] != 'H') {
-				x++;
-				status = Status.EMPTY;
-				return true;
-			} else {
-				status = Status.HOLE;
-				return false;
-			}
-		} else
+		if (x == N - 1)
 			return false;
+		else if (map[x][y] == 'X' || map[x][y] == 'Z')
+			return false;
+		else if (map[x + 1][y] == 'B')
+			return false;
+		else {
+			changPreviousCell();
+			x++;
+			changeCell();
+			return true;
+		}
 	}
 
 	// this method return void, it can return boolean
 	public boolean move_E() {
-		if (y != M) {
-			if (map[x][y] != 'H') {
-				y++;
-				status = Status.EMPTY;
-				return true;
-			} else {
-				status = Status.HOLE;
-				return false;
-			}
-		} else
+		if (y == M - 1)
 			return false;
+		else if (map[x][y] == 'X' || map[x][y] == 'Z')
+			return false;
+		else if (map[x][y + 1] == 'B')
+			return false;
+		else {
+			changPreviousCell();
+			y++;
+			changeCell();
+			return true;
+		}
 	}
 
 	// this method return void, it can return boolean
 	public boolean move_W() {
-		if (y != 0) {
-			if (map[x][y] != 'H') {
-				y--;
-				status = Status.EMPTY;
-				return true;
-			} else {
-				status = Status.HOLE;
-				return false;
-			}
-		} else
+		if (y == 0)
 			return false;
+		else if (map[x][y] == 'X' || map[x][y] == 'Z')
+			return false;
+		else if (map[x][y - 1] == 'B')
+			return false;
+		else {
+			changPreviousCell();
+			y--;
+			changeCell();
+			return true;
+		}
 	}
 	// ...
 
@@ -229,13 +312,23 @@ public class State {
 	// GOAL TESt: THIS WILL
 	// TELL WHETHER THE TREASURE WAS FOUND.
 	public boolean foundTreasure() {
-		// ...
+		if (map[x][y] == 'U' || map[x][y] == 'Z')
+			return true;
+		else
+			return false;
 	}
 
+	public boolean fallInHole() {
+		if (map[x][y] == 'X' || map[x][y] == 'Z')
+			return true;
+		else
+			return false;
+	}
 	// -----------------------------
 
 	// DISPLAY THE STATE
 	public void display() {
+		System.out.println("===========");
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
 				System.out.print(map[i][j]);
@@ -244,50 +337,42 @@ public class State {
 		}
 	}
 
+	public int getN() {
+		return N;
+	}
+
+	public int getM() {
+		return M;
+	}
+
 	// THIS METHOD WILL DO the GIVEN COMMAND
 	// AND WILL RETURN THE LOG MESSAGE
 	public String doCommandAndLog(String cmd) {
 		String log = "ERROR";
 		switch (cmd) {
 		case "move-N":
-			if (move_N()) {
+			if (move_N())
 				log = "DONE";
-			} else {
-				if (status == Status.EMPTY)
-					log = "FAIL";
-				else
-					log = "HOLE";
-			}
+			else
+				log = "FAIL";
 			break;
 		case "move-S":
-			if (move_S()) {
+			if (move_S())
 				log = "DONE";
-			} else {
-				if (status == Status.EMPTY)
-					log = "FAIL";
-				else
-					log = "HOLE";
-			}
+			else
+				log = "FAIL";
 			break;
 		case "move-E":
-			if (move_E()) {
+			if (move_E())
 				log = "DONE";
-			} else {
-				if (status == Status.EMPTY)
-					log = "FAIL";
-				else
-					log = "HOLE";
-			}
+			else
+				log = "FAIL";
 			break;
 		case "move-W":
-			if (move_W()) {
+			if (move_W())
 				log = "DONE";
-			} else {
-				if (status == Status.EMPTY)
-					log = "FAIL";
-				else
-					log = "HOLE";
-			}
+			else
+				log = "FAIL";
 			break;
 		}
 		return log;
@@ -308,23 +393,23 @@ public class State {
 
 	// THIS METHOD WILL RETURN THE SUCCESSOR STATES
 	// OF COURSE, YOU CAN & SHOULD CHANGE IT
-	public State[] successors() {
-		State children[] = new State[2]; // we have 2 actions
-
-		// action 1
-
-		children[0] = new State(this);
-		if (!children[0].action1())
-			children[0] = null;
-
-		// action 2
-
-		children[1] = new State(this);
-		if (!children[1].action1())
-			children[1] = null;
-
-		return children;
-	}
+	// public State[] successors() {
+	// State children[] = new State[2]; // we have 2 actions
+	//
+	// // action 1
+	//
+	// children[0] = new State(this);
+	// if (!children[0].action1())
+	// children[0] = null;
+	//
+	// // action 2
+	//
+	// children[1] = new State(this);
+	// if (!children[1].action1())
+	// children[1] = null;
+	//
+	// return children;
+	// }
 
 	// -----------------------------
 
